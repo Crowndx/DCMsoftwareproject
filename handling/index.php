c6671ddcfb788258c3398ded696d54bfd42207ba
David Walesby <david.j.walesby@gmail.com>
2019-03-08 18:06:40 -0500

    Add project files.

---
+++ handling/index.php
@@ -0,0 +1,68 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Handling Specialty - Custom Material Handling Equipment</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<script type="javascript" src="js/bootstrap.min.js"></script>
+		<link href="StyleSheet1.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+		<div id="header">
+			<div id="handlingLogo">
+				<img src="handlinglogo.png" />
+			</div>
+		</div>
+		<div id="main">
+			<div class="machineInfo">
+				<ul>
+					<li>Location</li>
+					<li>Machine</li>
+					<li>ON | OFF</li>
+					<li>Machine Status</li>
+					<li>D29:H12:M30:S10</li>
+					<li>D00:H11:M30:S50</li>
+				</ul>
+			</div>
+			<div class="customerInfo">
+				<div id="customerName">
+					<h2>Customer</h2>
+					<h3>Handling Specialty</h3>
+				</div>
+				<div id="machineLocation">
+					<h2>Machine Location</h2>
+					<h3>Hamilton, Ontario</h3>
+				</div>
+				<div id="function">
+					<h2>Function</h2>
+					<h3>Does this stuff</h3>
+				</div>
+				<div id="monitoring">
+					<h2>Monitoring</h2>
+					<h3>Temperature, Vibration, Runtime</h3>
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm">
+						<p>Motor 1</p>
+					</div>
+					<div class="col-sm">
+						<p>Motor 2</p>
+					</div>
+					<div class="col-sm">
+						<p>Motor 3</p>
+					</div>
+					<div class="col-sm">
+						<p>Motor 4</p>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+    </body>
+</html>
