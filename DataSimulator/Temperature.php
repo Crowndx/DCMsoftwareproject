c5604b84fc30a821ead35f264442ea6088619ed9
David Walesby <david.j.walesby@gmail.com>
2019-03-13 23:31:18 -0400

    Created Temperature Simulator and Vibration Simulator

---
+++ DataSimulator/Temperature.php
@@ -0,0 +1,24 @@
+<?php
+include 'DatabaseConnection.php';
+$runtime = 0;
+$machineOn = 1;
+$motorOn = 1;
+$motorID = 1;
+$counter = 0;
+while($machineOn === 1 && $motorOn === 1){
+    $counter = $counter + 1;
+    $runtime = $runtime + 1;
+    sleep(1);
+    $temperature = rand(15000,65000);
+    $date = date("Y/m/d h:i:s");
+    $sqlInsert = "INSERT INTO Temperature(TemperatureID,Temperature,DateTime,MotorID) VALUES(?,?,?,?)";
+    $params = array($counter,$temperature,$date,$motorID);
+    $insertStatement = sqlsrv_query($connection,$sqlInsert,$params);
+    if( $insertStatement === false ) {
+        die( print_r( sqlsrv_errors(), true));
+    }
+    if($counter > 10){
+        $machineOn = 0;
+    }
+}
+?>
