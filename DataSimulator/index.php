fc7aac70e012a2f6e96821c44507f418a8a7f1c2
David Walesby <david.j.walesby@gmail.com>
2019-03-13 22:52:13 -0400

    More testing of db inserts

--- DataSimulator/index.php
+++ DataSimulator/index.php
@@ -19,19 +19,10 @@ while($machineOn === 1 && $motorOn === 1){
     usleep(250000);
     $vibration = rand(28000,32000);
     $date = date("d/m/y h:i:s A");
-    /* Begin the transaction. */
-    if ( sqlsrv_begin_transaction( $connection ) === false ) {
-        die( print_r( sqlsrv_errors(), true ));
-    }
     $sqlInsert = "INSERT INTO Vibration(VibrationID,Vibration,DateTime,MotorID) VALUES(`$counter`, `$vibration`, `$date`, `$motorID`)";
     $insertStatement = sqlsrv_query($connection,$sqlInsert);
-    if($insertStatement){
-        sqlsrv_commit($connection);
-        echo "Vibrations added.<br />";
-    }
-    else{
-        sqlsrv_rollback($connection);
-        echo "Vibration not added.<br />";
+    if( $insertStatement === false ) {
+        die( print_r( sqlsrv_errors(), true));
     }
     if($counter > 10){
         $machineOn = 0;
